9022cc7a4c1ac1bfe0fa6d1e41e1448d22525c30
Erik <erska3276@oslomet.no>
2024-04-22 19:11:43 +0200

    added model sql-files (mvc-architecture)

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO Film (tittel) VALUES ( 'Last Samurai' );
+INSERT INTO Film (tittel) VALUES ( 'The Dark Knight' );
+INSERT INTO Film (tittel) VALUES ( 'The Godfather' );
+INSERT INTO Film (tittel) VALUES ( 'Edge of Tomorrow' );
+INSERT INTO Film (tittel) VALUES ( 'Nightcrawler' );
+INSERT INTO Film (tittel) VALUES ( 'V for Vendetta' );
+INSERT INTO Film (tittel) VALUES ( 'Con Air' );
+INSERT INTO Film (tittel) VALUES ( 'Kill Bill: Volume 2' );
+INSERT INTO Film (tittel) VALUES ( 'Die Hard' );
+INSERT INTO Film (tittel) VALUES ( 'Pacific Rim' );
+INSERT INTO Film (tittel) VALUES ( 'Dune Part 2' );
